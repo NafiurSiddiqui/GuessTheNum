cc19ccd9ac701a3e7aeedb97cb037550b5f5274f
TheHunter808 <saejim91@gmail.com>
2021-07-07 02:06:11 +0600

    sass and modal window functionality added

--- modalwindow.js
+++ modalwindow.js
@@ -1,1 +1,27 @@
-// const container = document.querySelector('');
+const container = document.querySelector('.modal_overlay_container');
+const btnChk = document.querySelector('.modal_overlay_container_input-check');
+
+const openModal = function(){
+    container.classList.remove('hidden')
+};
+
+const closeModal = function(){
+    container.classList.add('hidden')
+};
+
+btnChk.addEventListener('click',closeModal)
+
+document.addEventListener('DOMContentLoaded',function(){
+    openModal();
+});
+
+// container.addEventListener('click',closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Enter' && !container.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
